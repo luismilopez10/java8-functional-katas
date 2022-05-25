f78f9893cf04c33f85dc871d31258387564f83fd
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-25 16:46:22 -0500

    Kata 1 solved

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -7,6 +7,9 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+//import static java.util.stream.Nodes.collect;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -17,6 +20,9 @@ public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        return movies.stream()
+                .map(movie -> Map.of("id", movie.getId(), "title", movie.getTitle()))
+                .collect(Collectors.toList());
+//        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
     }
 }
