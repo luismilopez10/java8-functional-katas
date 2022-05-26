fe5766eef70ade914ec5796a18271dab7924719a
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-25 20:15:37 -0500

    Kata 6 solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,9 +18,9 @@ public class Main {
         assertEquals(Kata5.execute(), 5.0, 0.0);
         System.out.println("Kata 5 solved!");
 
-//        assertEquals(Kata6.execute(), "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg");
-//        System.out.println("Kata 6 solved!");
-//
+        assertEquals(Kata6.execute(), "http://cdn-0.nflximg.com/images/2891/Fracture300.jpg");
+        System.out.println("Kata 6 solved!");
+
 //        assertEquals(Kata7.execute().size(), 4);
 //        System.out.println("Kata 7 solved!");
 //
--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,11 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -14,6 +16,13 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        return movies.stream()
+                .map(movie -> movie.getBoxarts())
+                .flatMap(List::stream)
+                .reduce((boxart1, boxart2) -> boxart1.getWidth()*boxart1.getHeight() > boxart2.getWidth()*boxart2.getHeight() ? boxart1 : boxart2)
+                .map(BoxArt::getUrl)
+                .get();
+
+//        return "someUrl";
     }
 }
